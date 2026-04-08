2be32584ff13a3ba89d801b466aed3e3dbdce612
Rishikesh <rishirbagal@gmail.com>
2026-04-08 08:51:42 +0530

    chore: prepare backend for Render deployment

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000", "http://localhost:5173"],
     credentials: true,
     optionsSuccessStatus: 200
   })
@@ -28,7 +28,7 @@ const httpServer = createServer(app);
 // Initialize Socket.io with CORS config
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000", "http://localhost:5173"],
     methods: ['GET', 'POST'],
     credentials: true
   }
